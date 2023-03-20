0863a883890a45e7cebaad35d9cebbfb6c20604e
BadarauDavid <badaraudavidiulian@gmail.com>
2023-03-20 16:11:17 +0200

    la intamplare

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,58 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 import ReadLocation from "./Components/ReadLocation";
+import ScreenPokemon from "./Components/ScreenPokemon";
 
 function App() {
   const [locations, setLocation] = useState(null);
-  const [pokemon, setPokemon] = useState("https://pokeapi.co/api/v2/location");
+  const [pokemon, setPokemon] = useState([]);
   const [clicked, setClickState] = useState(true);
+
+  async function pokemonInfo (link){
+    try {
+      const response = await fetch(link);
+      const data = await response.json();
+      setPokemon(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+
+
   useEffect(() => {
     async function fetchLocation() {
       try {
-        const response = await fetch(pokemon);
+        const response = await fetch("https://pokeapi.co/api/v2/location");
         const data = await response.json();
-        console.log(data);
+        //   console.log(data);
         setLocation(data);
       } catch (error) {
         console.error(error);
       }
     }
     fetchLocation();
-  }, [pokemon]);
-  console.log(locations);
+  }, []);
+  
+  const clickHandle = async(e) => {
+    try {
+      const response = await fetch("https://pokeapi.co/api/v2/location-area/" + e.target.id);
+      const data = await response.json();
+      // console.log(data.pokemon_encounters[Math.floor(Math.random() * data.pokemon_encounters.length)].pokemon.url);
+      setLocation(data);
+      setClickState(false);
+      await pokemonInfo(data.pokemon_encounters[Math.floor(Math.random() * data.pokemon_encounters.length)].pokemon.url)
 
-  const clickHandle = (e) => {
-    setPokemon("https://pokeapi.co/api/v2/location-area/" + e.target.id);
-    console.log(pokemon);
-    setClickState(false);
+      
+      // takePhoto(data.pokemon_encounters[Math.floor(Math.random() * data.pokemon_encounters.length)].pokemon.url)
+    } catch (error) {
+      console.error(error);
+    }
+    
+
     //console.log("https://pokeapi.co/api/v2/location-area/" + e.target.id);
   };
+  console.log(pokemon.sprites)
 
   return (
     <div className="App">
@@ -41,7 +67,11 @@ function App() {
           />
         ))
       ) : (
-        <p>bravo merge</p>
+        <ScreenPokemon 
+        photo ={pokemon.sprites.front_default}
+        name = {pokemon.name}
+        />
+
       )}
     </div>
   );
---
+++ client/src/Components/ScreenPokemon.js
@@ -0,0 +1,17 @@
+function ScreenPokemon(props){
+    const photo = props.photo;
+    const name = props.name
+    return (
+
+
+        <div >
+        <h2>{name}</h2>
+        <img src={photo} />
+        </div> 
+        
+      
+    )
+}
+
+export default ScreenPokemon ; 
+
