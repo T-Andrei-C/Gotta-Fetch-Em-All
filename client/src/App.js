5edcde63073baa7827cce66f856eada5841d0c53
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2023-03-21 13:03:24 +0200

    Task 2 finished

--- client/src/App.js
+++ client/src/App.js
@@ -6,23 +6,24 @@ import NextAndPrevButton from "./Components/NextAndPrevButton";
 import NoPokemonAvailable from "./NoPokemonAvaible";
 
 function App() {
-  const [linkLocation, setLinkLocation] = useState("https://pokeapi.co/api/v2/location")
+  const [linkLocation, setLinkLocation] = useState(
+    "https://pokeapi.co/api/v2/location"
+  );
   const [locations, setLocation] = useState(null);
   const [pokemon, setPokemon] = useState([]);
   const [clicked, setClickState] = useState(true);
-  const [NoPokemon , setNoPokemon] = useState(true);
+  const [NoPokemon, setNoPokemon] = useState(true);
 
   async function locationInfo(link) {
     try {
       const response = await fetch(link);
       const data = await response.json();
-     // console.log(data.areas[Math.floor(Math.random() * data.areas.length)].url)
-     if(data.areas.length>0)
-     {return(data.areas[Math.floor(Math.random() * data.areas.length) ].url)}
-     else setNoPokemon(false)
-      
-     
-     // return data;
+      // console.log(data.areas[Math.floor(Math.random() * data.areas.length)].url)
+      if (data.areas.length > 0) {
+        return data.areas[Math.floor(Math.random() * data.areas.length)].url;
+      } else setNoPokemon(false);
+
+      // return data;
     } catch (error) {
       console.error(error);
     }
@@ -55,14 +56,12 @@ function App() {
     try {
       const response = await fetch(link);
       const data = await response.json();
-      console.log(data.pokemon_encounters.length)
-      if(data.pokemon_encounters.length>0){
+      console.log(data.pokemon_encounters.length);
+      if (data.pokemon_encounters.length > 0) {
         return data.pokemon_encounters[
           Math.floor(Math.random() * data.pokemon_encounters.length)
         ].pokemon.url;
-      }
-      else setNoPokemon(false)
-
+      } else setNoPokemon(false);
     } catch (error) {
       console.error(error);
     }
@@ -72,49 +71,50 @@ function App() {
   // console.log(pokemon.sprites)
 
   const a = async (location) => {
-    const l = await locationInfo(location)
+    const l = await locationInfo(location);
     // await locationInfo(location)
     const b = await clickHandle(l);
     const z = await pokemonInfo(b);
     setPokemon(z);
     setClickState(false);
-
   };
-
 
   return (
     <div className="App">
-      {clicked?(
-         <NextAndPrevButton 
-         nextHendle = {()=>setLinkLocation(locations.next)}
-         prevHendle = {()=>setLinkLocation(locations.previous)}
-            />
-      ):""}
+      {clicked ? (
+        <NextAndPrevButton
+          nextHendle={() => setLinkLocation(locations.next)}
+          prevHendle={() => setLinkLocation(locations.previous)}
+        />
+      ) : (
+        ""
+      )}
 
-      { clicked ? (
+      {clicked ? (
         locations &&
         locations.results.map((location, index) => (
           <ReadLocation
             name={location.name}
-            clickFunction={()=>a(location.url)}
+            clickFunction={() => a(location.url)}
             id={index + 1}
             key={index}
           />
         ))
-      ) : ( NoPokemon ?
-(        <ScreenPokemon
+      ) : NoPokemon ? (
+        <ScreenPokemon
           photo={pokemon.sprites.other.dream_world.front_default}
           name={pokemon.name}
-        />): <NoPokemonAvailable 
-        backHandle = {()=>{setNoPokemon(true); 
-                         setClickState(true);
-                         setLinkLocation(locations.next) 
-                        }}
+        />
+      ) : (
+        <NoPokemonAvailable
+          backHandle={() => {
+            setNoPokemon(true);
+            setClickState(true);
+          }}
         />
       )}
     </div>
   );
 }
-
 
 export default App;
