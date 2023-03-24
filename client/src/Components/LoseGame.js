0b32d286938c257a96cb767934f52bd1392e457a
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2023-03-24 10:10:05 +0200

    LoseGame component

--- client/src/Components/LoseGame.js
+++ client/src/Components/LoseGame.js
@@ -1,12 +1,11 @@
 function loseGame(props) {
-    const back = props.back;
-    return (
-      <div>
-        <p>You lost the battle!!! Better luck next time!</p>
-        <button onClick={back}>Try again</button>
-      </div>
-    );
-  }
-  
-  export default loseGame;
-  
+  const back = props.back;
+  return (
+    <div className="loseGame">
+      <p>You lost the battle!!! Better luck next time!</p>
+      <button onClick={back}>Try again</button>
+    </div>
+  );
+}
+
+export default loseGame;
