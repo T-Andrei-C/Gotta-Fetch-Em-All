91c7918b52d3ed76f0961b097a83edef95b4ba4a
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2023-03-24 10:13:49 +0200

    WinGame component

--- client/src/Components/WinGame.js
+++ client/src/Components/WinGame.js
@@ -1,12 +1,11 @@
 function winGame(props) {
-    const back = props.back;
-    return (
-      <div>
-        <img src={"https://www.svgrepo.com/show/276264/pokeball-pokemon.svg"} />
-        <button onClick={back}>Advance to next battle</button>
-      </div>
-    );
-  }
-  
-  export default winGame;
-  
+  const back = props.back;
+  return (
+    <div>
+      <img src={"https://www.svgrepo.com/show/276264/pokeball-pokemon.svg"} />
+      <button onClick={back}>Advance to next battle</button>
+    </div>
+  );
+}
+
+export default winGame;
