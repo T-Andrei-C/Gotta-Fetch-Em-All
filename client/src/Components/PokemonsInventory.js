f8bd50adfcc60d6473df2ec2542a2e529f204364
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2023-03-24 10:13:18 +0200

    PokemonInventory component

--- client/src/Components/PokemonsInventory.js
+++ client/src/Components/PokemonsInventory.js
@@ -1,39 +1,19 @@
 function pokemonsInventory(props) {
-  const pokemonName =
-    props.pokemonName.charAt(0).toUpperCase() + props.pokemonName.slice(1);
+  const pokemonName = props.pokemonName;
   const pokemonPhoto = props.pokemonPhoto;
   const clickHandler = props.chosenPokemonClick;
-  const attack = props.attack;
-  const health = props.health;
-  const defense = props.defense;
+  const attack = props.attack
+  const health = props.health
+  const defense = props.defense
   return (
-    <div className="col">
-      <div className="card">
-        <img
-          onClick={clickHandler}
-          src={pokemonPhoto}
-          className="card-img-top"
-        />
-        <div className="card-body">
-          <h5 className="card-title">{pokemonName}</h5>
-        </div>
-        <ul className="list-group list-group-flush">
-          <li className="list-group-item">Attack: {attack}</li>
-          <li className="list-group-item">Health: {health}</li>
-          <li className="list-group-item">Defense: {defense}</li>
-        </ul>
-      </div>
+    <div className="inventory">
+      <h2>{pokemonName}</h2>
+      <img onClick={clickHandler} src={pokemonPhoto} />
+      <h3>Attack: {attack}</h3>
+      <h3>Health: {health}</h3>
+      <h3>Defense: {defense}</h3>
     </div>
   );
 }
 
 export default pokemonsInventory;
-{
-  /* <div className="col">
-      <label>{pokemonName}</label>
-      <img onClick={clickHandler} src={pokemonPhoto} />
-      <p>Attack: {attack}</p>
-      <p>Health: {health}</p>
-      <p>Defense: {defense}</p>
-    </div> */
-}
