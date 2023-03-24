e5b672bfc2e2a1aad55d0498e084fa5d17ddad6c
T-Andrei-C <115529065+T-Andrei-C@users.noreply.github.com>
2023-03-24 10:12:26 +0200

    Pokemon Encounter component

--- client/src/Components/PokemonEncounter.js
+++ client/src/Components/PokemonEncounter.js
@@ -2,12 +2,20 @@ function pokemonEncounter(props) {
   const photo = props.photo;
   const name = props.name;
   const health = props.health
+  const damagedHealth = props.damagedHealth
+  console.log()
   return (
-    <div>
+    <>
+    <div className="encounterPokemon">
       <h2>{name}</h2>
       <img src={photo} />
-      <p>Health: {health}</p>
+      <h3>Health: {health}</h3>
+      <progress id="hpEncounter" value={health} max={damagedHealth}></progress>
     </div>
+    <div className="versus">
+      <img src="https://cdn.onlinewebfonts.com/svg/img_418591.png"/>
+    </div>
+    </>
   );
 }
 
